e8aa6fbd5d9d9dec8c218a1d8dbc69e0ed0d5a79
Epicsociety <measkswho@gmail.com>
2022-10-14 20:39:56 -0700

    Betty does not like space

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-int main ()
+/**
+ * main - prints a string of char with printf
+ *
+ * Return: 0
+*/
+int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
-	return 0;
+	return (0);
 }
