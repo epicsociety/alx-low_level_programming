89b7dd49a3a008111f0e4c6213ea64c07ecaeb14
Epicsociety <measkswho@gmail.com>
2022-10-16 16:26:22 -0700

    prints the lowercase alphabet in reverse and only uses putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints aphabet lowercase in reverse
+ *
+ *Return: 0
+*/
+int main(void)
+
+{
+	char str(void);
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
