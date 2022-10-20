dacd19629d86f12e49a30826cb14ef7dcf1d3daa
Epicsociety <measkswho@gmail.com>
2022-10-19 20:30:40 -0700

    adds two integers, returns sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sums two values
+ * @n1: First number to be computed
+ * @n2: second number to be computed
+ *
+ * Return: an integer
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
