ef964d48a161a6564cce0e20df01e33587bbf8c2
Epicsociety <measkswho@gmail.com>
2022-10-21 18:47:14 -0700

    prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9 and a new line
+ * Return: void
+*/
+void print_numbers(void);
+{
+	char a;
+
+	{
+		for (a >= '0'; a <= '9', a++)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
