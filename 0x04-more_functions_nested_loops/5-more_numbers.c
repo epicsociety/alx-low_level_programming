6363ea633b2eb3b292b3d7a60f2417f47008f4b6
Epicsociety <measkswho@gmail.com>
2022-10-24 03:54:42 -0700

    function that draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,12 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 14; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-	_putchar('\n');
+		j = 1;
+		while (j <= 10)
+			_putchar(i);
+		j++;
 	}
+	_putchar('\n');
 }
