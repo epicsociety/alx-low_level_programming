a42e362150a351bfb669b4438a3e9fcc5f38c570
Epicsociety <measkswho@gmail.com>
2022-11-07 20:49:09 +0300

    argc, and argv. Program that prints it's own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 
 /**
  * main - prints the program's name
- *
+ * @argc: gives the argument count pased to main in command line
+ * @argv: a string that stores the arguments
  * Return: 0
 */
 int main(int argc, char *argv[])
