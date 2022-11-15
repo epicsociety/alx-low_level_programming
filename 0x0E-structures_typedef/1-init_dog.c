5c56e0fa32d382aaf7cc30264b71a6c0a43dd6dd
Epicsociety <measkswho@gmail.com>
2022-11-15 23:23:22 +0300

    IPointers and structres

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog -  initializes a variable of type struct dog
+ *@d: is the initalization
+ *@name: is the member of the struct
+ *@age: is the second member of the struct
+ *@owner: is the third member
+
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		(*d).owner = owner;
+	}
+}
