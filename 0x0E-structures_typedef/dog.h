14ec7417ed32e204f7aa7f94d2c11a69cf0708ba
Epicsociety <measkswho@gmail.com>
2022-11-15 22:57:12 +0300

    struct dog >>

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a simple struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+*/
+
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
