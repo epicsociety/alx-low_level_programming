918f0520aeaeecefb71ae1117de0d538b34a3726
Epicsociety <measkswho@gmail.com>
2022-11-18 03:14:09 +0300

    Added function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an interger
+ * @size: the number of elements in the array
+ * @cmp: a pointer to the function to be used to compare values
+ * @array: pointer to the array to be used
+ *
+ * Return: index of the first element for
+ * which the cmp function does not return 0
+ * -1 if no element matches
+ * If size <= 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t index;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (index = 0; index < size; index++)
+	{
+		if ((*cmp)(array[index]) != 0)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
