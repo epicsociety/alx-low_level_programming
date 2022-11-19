e18d582af0e20a06e4a3482ea46a96e8ea6cacaa
Epicsociety <measkswho@gmail.com>
2022-11-19 04:35:01 +0300

    A function that prints numbers passed as args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers, passed as args
+ * @separator: the separator
+ * @n: number of arguments passed
+ *
+ * Return: 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arguments;
+	unsigned int i;
+
+	va_start(arguments, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int value = va_arg(arguments, const unsigned int);
+
+		if (separator == NULL)
+		{
+
+		}
+		if (i != n)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", value);
+	}
+	printf("\n");
+	va_end(arguments);
+}
