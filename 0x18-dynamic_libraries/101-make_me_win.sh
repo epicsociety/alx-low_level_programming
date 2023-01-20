bdde88b75dd9dbef25f7d2c29f1235cc7cccdc14
Epicsociety <measkswho@gmail.com>
2023-01-20 20:16:55 +0300

    Checks directory, and Congratulations, you win the Jackpot!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-bash -c "history -c; rm 101-make_me_win.sh; ls -la; ./gm 9 8 10 24 75 9"
+#!/bin/sh
+./gm 9 8 10 24 75 9
+sleep 5
+history -c
+sleep 5
+rm 101-make_me_win.sh
+sleep 5
+ls -la
