3cb3d34bcc70dc9e19de168105c3fe0f392d7d60
Epicsociety <measkswho@gmail.com>
2023-05-09 21:53:18 +0300

    linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - searches for a value in an array using linear search
+ * @array: pointer to the first element in the array
+ * @size: the size of the array
+ * @value: what the function is searching for
+ *
+ *Return: 0 on success and -1 on failure
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
+			return (i);
+		}
+
+		printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
+	}
+	if (i == size)
+	{
+		return (-1);
+	}
+	else
+		return (0);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,10 @@
+# search algorithms
+
+## linear search
+> A linear search algorithm is a simple approach for searching an item in a list. It sequentially checks each element of the list until a match is found.
+## binary search 
+>Binary search algorithm is an efficient search algorithm that is used to search an item in a sorted array. It divides the array into half at each step and compares the key with the middle element of the array. If the key is smaller than the middle element, the search is performed on the left half of the array, otherwise, the search is performed on the right half of the array.
+## Jump search algorithm
+##  Interpolation search algorithm
+## Exponential search algorithm
+
